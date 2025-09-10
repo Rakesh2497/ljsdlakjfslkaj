98cf2cda3b064b160f12930556d2a821243ba456
Akhil <nanankal@uab.edu>
2025-09-10 14:36:14 -0500

    Added some extra validations

--- readcsv.py
+++ readcsv.py
@@ -1,10 +1,33 @@
 import pandas as pd
 
+
 # Path to your CSV file
 file_path = "/Users/rakeshnanankal/Downloads/Electric_Vehicle_Population_Data.csv"
 
 # Read CSV into a Pandas DataFrame
-df = pd.read_csv(file_path, delimiter=',', usecols=[0,2,3,4], nrows=100)
+df = pd.read_csv(file_path)
 
 # Display the first 5 rows
-print(df.iloc[8])
+print(df.head())
+print(df.columns)
+# Display the number of rows and columns
+print(df.shape)
+# Drop NaN values
+df = df.dropna()
+# Display the number of rows and columns after dropping NaN values
+print(df.shape)
+# Display data types of each column
+print(df.dtypes)
+# Display summary statistics
+print(df.describe())
+# Display unique values in a specific column (e.g., 'Make')
+print(df['Make'].unique())
+# Display the count of unique values in a specific column (e.g., 'Make')
+print(df['Make'].value_counts())
+# Filter rows based on a condition (e.g., 'Make' is 'TESLA')
+tesla_cars = df[df['Make'] == 'TESLA']
+print(tesla_cars.count())
+
+
+
+
